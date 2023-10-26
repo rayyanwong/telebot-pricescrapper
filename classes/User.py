47a691a62ac74cc351755573c74ad8f08b7a19f5
rayyan <rwrayyan2004@gmail.com>
2023-10-26 09:43:48 +0800

    Added get and add functions sql to User table

--- classes/User.py
+++ classes/User.py
@@ -6,71 +6,111 @@ from sqlalchemy import update
 session = Session(bind=engine)
 
 
+def errormsg(function_name: str):
+    print(f"Error has occured while executing {function_name}")
+
+
 class User:
     def __init__(self):
         self.name = "Rayyan tele bot"
 
     def buff_watchlist_add(self, userid, item):
-        selected = session.query(UserDB).filter_by(
-            userid=userid).first()
-        templist = selected.buff_watchlist
-        print("Initial: ", templist)
-        templist.append(item)
-        print("After: ", templist)
-        session.execute(update(UserDB).where(UserDB.userid ==
-                        userid).values(buff_watchlist=templist))
-        session.commit()
+        try:
+            selected = session.query(UserDB).filter_by(
+                userid=userid).first()
+            templist = selected.buff_watchlist
+            print("Initial: ", templist)
+            templist.append(item)
+            print("After: ", templist)
+            session.execute(update(UserDB).where(UserDB.userid ==
+                            userid).values(buff_watchlist=templist))
+            session.commit()
+        except Exception as e:
+            errormsg("buff_watchlist_add")
+        return
 
     def get_buff_watchlist(self, userid):
-        ret = session.query(UserDB).filter(
-            UserDB.userid == userid).one().buff_watchlist
-        return ret
+        try:
+            ret = session.query(UserDB).filter(
+                UserDB.userid == userid).one().buff_watchlist
+            return ret
+        except Exception as e:
+            errormsg("get_buff_watchlist")
+            return
 
-    # def stock_watchlist_add(self, item):
-    #     self.stock_watchlist.append(item)
-    #     return
+    def stock_watchlist_add(self, userid: int, item: object):
+        try:
+            selected = session.query(UserDB).filter_by(
+                userid=userid).first()
+            templist = selected.stock_watchlist
+            print("Initial: ", templist)
+            templist.append(item)
+            print("After: ", templist)
+            session.execute(update(UserDB).where(UserDB.userid ==
+                            userid).values(stock_watchlist=templist))
+            session.commit()
+        except Exception as e:
+            errormsg("stock_watchlist_add")
 
-    # def buff_watchlist_remove(self, item):
-    #     try:
-    #         self.buff_watchlist.remove(item)
-    #         return "Success"
-    #     except Exception as e:
-    #         return "Failed to remove item from Buff watchlist!"
+    def get_stock_watchlist(self, userid: int, item: object):
+        try:
+            ret = session.query(UserDB).filter(
+                UserDB.userid == userid).one().stock_watchlist
+            return ret
+        except Exception as e:
+            errormsg("get_stock_watchlist")
 
-    # def stock_watchlist_remove(self, item):
-    #     try:
-    #         self.stock_watchlist.remove(item)
-    #         return "Success"
-    #     except Exception as e:
-    #         return "Failed to remove item from Stock watchlist!"
+    def buff_investments_add(self, userid: int, item: object):
+        try:
+            selected = session.query(UserDB).filter_by(userid=userid).first()
+            templist = selected.buff_investments
+            print("Initial: ", templist)
+            templist.append(item)
+            print("After: ", templist)
+            session.execute(update(UserDB).where(UserDB.userid ==
+                            userid).values(buff_investments=templist))
+            session.commit()
+        except Exception as e:
+            errormsg("buff_investments_add")
 
-    # def buff_investment_add(self, item):
-    #     self.buff_investments.append(item)
-    #     return
+    def get_buff_investments(self, userid: int):
+        try:
+            ret = session.query(UserDB).filter(
+                UserDB.userid == userid).one().buff_investments
+            return ret
+        except Exception as e:
+            errormsg("get_buff_investments")
 
-    # def buff_investment_remove(self, item):
-    #     try:
-    #         self.buff_investments.remove(item)
-    #         return "Success"
-    #     except Exception as e:
-    #         return "Failed to remove item from Buff investments"
+    def stock_investments_add(self, userid: int, item: object):
+        try:
+            selected = session.query(UserDB).filter_by(userid=userid).first()
+            templist = selected.stock_investments
+            print("Initial: ", templist)
+            templist.append(item)
+            print("After: ", templist)
+            session.execute(update(UserDB).where(UserDB.userid ==
+                            userid).values(stock_investments=templist))
+            session.commit()
+        except Exception as e:
+            errormsg("stock_investments_add")
 
-    # def stock_investment_add(self, item):
-    #     self.stock_investments.append(item)
-    #     return
+    def get_stock_investments(self, userid: int):
+        try:
+            ret = session.query(UserDB).filter(
+                UserDB.userid == userid).one().stock_investments
+            return ret
+        except Exception as e:
+            errormsg("get_stock_investments")
 
-    # def stock_investment_remove(self, item):
-    #     try:
-    #         self.stock_investments.remove(item)
-    #         return "Success"
-    #     except Exception as e:
-    #         return "Failed to remove item from Stock investments"
-
-    # def get_stock_watchlist(self):
-    #     return self.stock_watchlist
-
-    # def get_buff_investments(self):
-    #     return self.buff_investments
-
-    # def get_stock_investments(self):
-    #     return self.stock_investments
+    def buff_watchlist_remove(self, userid: int, item: object):
+        try:
+            selected = session.query(UserDB).filter_by(userid=userid).first()
+            templist = selected.buff_watchlist
+            print("Initial: ", templist)
+            templist.remove(item)
+            print("After: ", templist)
+            session.execute(update(UserDB).where(UserDB.userid ==
+                            userid).values(buff_watchlist=templist))
+            session.commit()
+        except Exception as e:
+            errormsg("buff_watchlist_remove")
